11170514706db2d3f577acec9792b0268cf56b08
Steve <designerofthings@gmail.com>
2020-03-19 11:56:00 +0100

    complete fizz, buzz, fizzbuzz test

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,8 +1,17 @@
 def fizz_buzz(number)
-    if number % 3 == 0
+    if number % 15 == 0
+        'fizzbuzz'
+    elsif number % 5 == 0
+        'buzz'
+    elsif number % 3 == 0
         'fizz'
     else
         number
     end
     
 end
+#Review and reflect answers
+#1. by directing the flow of the code to control the data's journey through it
+#2. Case statement
+#3.the modulus returns the remained after a number is divided by another number
+#4. = is used to assign a value, == is equal to
